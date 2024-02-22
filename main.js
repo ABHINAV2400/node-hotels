69a97f86c92797949cf32d056d44bfc3984f2992
ABHINAV2400 <bollaabhinav2412@gmail.com>
2024-02-22 14:16:49 +0100

    mongo db changes

--- main.js
+++ main.js
@@ -23,6 +23,7 @@
 const express = require('express')
 const app = express();
 const db = require('./db')
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -31,6 +32,8 @@ const personRoutes = require('./routes/person_routes');
 app.use('/person',personRoutes); 
 
 const MenuItem = require('./models/menuItem')
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
     res.send('hello world');
